1be1942149d746d0068f24217394d3d28968aaee
Jens Kober <j.kober@tudelft.nl>
2022-03-10 17:24:02 +0100

    filtfilt is now part of scipy

--- support_funcs/regr_data_proc.py
+++ support_funcs/regr_data_proc.py
@@ -4,72 +4,8 @@ import scipy.signal
 import pickle
 
 
-def filtfilt_simple(b, a, input_vector):
-    '''input_vector has shape (n,1)'''
-    forward = scipy.signal.lfilter(b, a, input_vector, axis=0)
-    return scipy.flipud( scipy.signal.lfilter(b, a, scipy.flipud(forward), axis = 0) )
-
-
-def lfilter_zi(b,a):
-    #compute the zi state from the filter parameters. see [Gust96].
-
-    #Based on:
-    # [Gust96] Fredrik Gustafsson, Determining the initial states in forward-backward 
-    # filtering, IEEE Transactions on Signal Processing, pp. 988--992, April 1996, 
-    # Volume 44, Issue 4
-
-    n=max(len(a),len(b))
-
-    zin = (  numpy.eye(n-1) - numpy.hstack( (-a[1:n,numpy.newaxis],
-                                 numpy.vstack((numpy.eye(n-2), numpy.zeros(n-2))))))
-
-    zid=  b[1:n] - a[1:n]*b[0]
-
-    zi_matrix=numpy.linalg.inv(zin)*(numpy.matrix(zid).transpose())
-    zi_return=[]
-
-    #convert the result into a regular array (not a matrix)
-    for i in range(len(zi_matrix)):
-      zi_return.append(float(zi_matrix[i][0]))
-
-    return numpy.array(zi_return)
-
-
-def filtfilt(b,a,x):
-    #For now only accepting 1d arrays
-    ntaps=max(len(a),len(b))
-    edge=ntaps*3
-
-    if x.ndim != 1:
-        raise ValueError("Filiflit is only accepting 1 dimension arrays.")
-
-    #x must be bigger than edge
-    if x.size < edge:
-        raise ValueError("Input vector needs to be bigger than 3 * max(len(a),len(b).")
-
-    if len(a) < ntaps:
-        a=numpy.r_[a,numpy.zeros(len(b)-len(a))]
-
-    if len(b) < ntaps:
-        b=numpy.r_[b,numpy.zeros(len(a)-len(b))]
-
-    zi=scipy.signal.lfilter_zi(b,a)
-
-    #Grow the signal to have edges for stabilizing 
-    #the filter with inverted replicas of the signal
-    s=numpy.r_[2*x[0]-x[edge:1:-1],x,2*x[-1]-x[-1:-edge:-1]]
-    #in the case of one go we only need one of the extrems 
-    # both are needed for filtfilt
-
-    (y,zf)=scipy.signal.lfilter(b,a,s,-1,zi*s[0])
-
-    (y,zf)=scipy.signal.lfilter(b,a,numpy.flipud(y),-1,zi*y[-1])
-
-    return numpy.flipud(y[edge-1:-edge+1])
-
-
 def butter_lfilter( N, Wn, signal):
-    butter_b,butter_a = scipy.signal.butter(  N, Wn  )
+    butter_b,butter_a = scipy.signal.butter( N, Wn )
     filtered_signal = scipy.signal.lfilter(butter_b,butter_a,signal)
     return filtered_signal
 
@@ -78,8 +14,8 @@ _inf = float('inf')
 
 def butter_filtfilt( N, Wn, signal):
     if Wn == _inf: return signal[:]
-    butter_b,butter_a = scipy.signal.butter(  N, Wn  )
-    filtered_signal = filtfilt(butter_b,butter_a,signal)
+    butter_b,butter_a = scipy.signal.butter( N, Wn )
+    filtered_signal = scipy.signal.filtfilt(butter_b,butter_a,signal)
     return filtered_signal
 
 
